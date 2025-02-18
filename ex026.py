d70d1f8e9a6bbc508f776fd0de5dd88a57cf32de
Jaqueline <jaqueline.jsgc@gmail.com>
2025-02-18 07:35:59 -0300

    Python exercise working on: String Input and Processing, Character Counter, Finding the First and Last Occurrence of a Character.

--- ex026.py
+++ ex026.py
@@ -1,3 +1,5 @@
-frase = str (input('Digite uma frase: ')).strip()
-frase.upper()
-print('O número de vezes que a letra A aparece na frase é {}'.format(frase.find('A')))
+frase = str (input('Digite uma frase: ')).strip().upper()
+print('O número de vezes que a letra A aparece na frase é {}'.format(frase.count('A')))
+print('A letra A aparece a primeira vez na posição {}'.format(frase.find('A')+1))
+print('A última posição da letra A é {}'.format(frase.rfind('A')+1))
+
